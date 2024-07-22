8834f52c1826c0799b243fdd4000d3307d0e032f
Aleks9158 <acca5859@gmail.com>
2024-07-22 16:03:35 +0300

    class Main renew

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -24,6 +24,11 @@ public class Main {
         FacX.size="9999999999999m";
 
         //adding the features for planet1
+        Planet1Features GulK=new Planet1Features();
+        GulK.color="blue";
+        GulK.size= "3848325828km";
+        GulK.name="pracheX";
+
 
     }
 }
